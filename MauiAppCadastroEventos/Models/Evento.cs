cc212012a5e63bb942b34fc2e654f5a6ec48e2db
Lucas <lsoaresbarbosa20@gmail.com>
2026-06-01 08:23:05 -0300

    ajustes

---
+++ MauiAppCadastroEventos/Models/Evento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppCadastroEventos.Models
+{
+    public class Evento
+    {
+        public string NomeEvento { get; set; }
+
+        public DateTime DataCheckin { get; set; }
+
+        public DateTime DataCheckout { get; set; }
+
+        public TimeSpan Duracao
+        {
+            get => DataCheckout.Subtract(DataCheckin);
+        }
+
+        public int QntParticipantes { get; set; }
+        public int CustoParticipante { get; set; }
+
+        public string Local {  get; set; }
+
+        public double Total
+        {
+            get
+            {
+                double valor = QntParticipantes * CustoParticipante;
+                return valor;
+            }
+
+        }
+    }
+}
--- MauiAppCadastroEventos/Views/HomePage.xaml.cs
+++ MauiAppCadastroEventos/Views/HomePage.xaml.cs
@@ -1,9 +1,59 @@
 namespace MauiAppCadastroEventos.Views;
 
+using MauiAppCadastroEventos.Models;
+using Microsoft.Maui.Controls;
+
 public partial class HomePage : ContentPage
 {
-	public HomePage()
+    
+    public HomePage()
 	{
 		InitializeComponent();
+
+		DatePicker_checkin.Date = DateTime.Now;
+		DatePicker_checkin.MinimumDate = DateTime.Now;
+		DatePicker_checkin.MaximumDate = DateTime.Today.AddMonths(6);
+
+		DatePicker_checkout.MinimumDate = DatePicker_checkin.Date.Value.AddDays(1);
+		DatePicker_checkout.MaximumDate = DatePicker_checkin.Date.Value.AddDays(7);
 	}
+
+    private void DatePicker_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		try
+		{
+			DatePicker elemento = sender as DatePicker;
+
+			DateTime? Checkin_dataSelecionada = elemento.Date;
+
+			DatePicker_checkout.MinimumDate = Checkin_dataSelecionada.Value.AddDays(1);
+			DatePicker_checkout.MaximumDate = Checkin_dataSelecionada.Value.AddDays(7);
+
+		} catch (Exception ex)
+		{
+			DisplayAlertAsync("Erro", ex.Message, "Ok");
+		}
+
+
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Evento evento = new Evento
+			{
+				NomeEvento = txt_NomeEvento.Text,
+				DataCheckin = DatePicker_checkin.Date.Value,
+				DataCheckout = DatePicker_checkout.Date.Value,
+				QntParticipantes = Convert.ToInt32(stepper_participantes.Value),
+				
+
+				
+
+			}
+
+        }
+    }
 }
