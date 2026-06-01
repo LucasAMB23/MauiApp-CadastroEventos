a8073a34fb1bdda2bc740cc2794ab2387b0ca259
Lucas <lsoaresbarbosa20@gmail.com>
2026-05-31 22:59:42 -0300

    Adicionando campos iniciais

--- MauiAppCadastroEventos/App.xaml.cs
+++ MauiAppCadastroEventos/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MauiAppCadastroEventos.Views;
 
 namespace MauiAppCadastroEventos
 {
@@ -11,7 +12,7 @@ namespace MauiAppCadastroEventos
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new NavigationPage(new HomePage()));
         }
     }
 }
---
+++ MauiAppCadastroEventos/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppCadastroEventos.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
